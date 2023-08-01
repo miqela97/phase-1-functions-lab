f6838877334fa333d4afa482c223d50b51139a94
miqela97 <132955511+miqela97@users.noreply.github.com>
2023-08-01 11:02:32 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - 42);
+}
+function distanceFromHqInFeet(street) {
+  const feetPerBlock = 264;
+  return Math.abs(street - 42) * feetPerBlock;
+}
+function distanceTravelledInFeet(start, destination) {
+  const feetPerBlock = 264;
+  return Math.abs(start - destination) * feetPerBlock;
+}
+function calculatesFarePrice(start, destination){
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance < 400) return  0;
+  if (distance > 400 && distance < 2000) return (distance - 400) * .02;
+  if (distance > 2000 && distance < 2500 ) return 25;  
+  if (distance > 2500) return "cannot travel that far";
+}
